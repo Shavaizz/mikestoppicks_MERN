152ec34e617a5d87981e218ef49a0e7877939932
Shavaiz <shaviz4444@gmail.com>
2025-02-11 01:08:59 +0500

    cart page final cart-items, order placing sidebar not existing, component to be made

--- frontend/src/components/CartItems/CartItems.jsx
+++ frontend/src/components/CartItems/CartItems.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import api from "../../axiosinstance";
-const CartItems = ({user}) => {
+import "./CartItems.css";
+const CartItems = ({ user }) => {
 	const [cartObjects, setCartObjects] = useState([]);
 	useEffect(() => {
 		const fetchCartObjects = async () => {
@@ -8,7 +9,7 @@ const CartItems = ({user}) => {
 				const response = await api.get(
 					`http://localhost:3000/api/cart/${user.user?.id}`
 				);
-                console.log(user?.user);
+				console.log(user?.user);
 				setCartObjects(response.data.items);
 			} catch (error) {
 				console.error("Error fetching cart objects:", error);
@@ -21,21 +22,20 @@ const CartItems = ({user}) => {
 			<div className="cart-body">
 				{cartObjects.length > 0 ? (
 					cartObjects.map((item) => (
-						<div key={item.productId._id} className="cart-item">
-							<img
-								src={item.productId.image}
-								alt={item.productId.title}
-								className="cart-item-image"
-							/>
-							<div className="cart-item-details">
-								<h3>{item.productId.title}</h3>
-								<p>Price: ${item.productId.price}</p>
-								<p>Quantity: {item.quantity}</p>
-								<p>Total Price: ${item.productId.price * item.quantity}</p>
-							</div>
-							<div className="cart-page-actions">
-								<button type="button">+</button>
-								<button type="button">-</button>
+						<div className="cart-item-wrapper">
+							<div key={item.productId._id} className="cart-item">
+								<img
+									src={item.productId.image}
+									alt={item.productId.title}
+									id="cart-item-image"
+								/>
+								<div className="cart-item-details">
+                                    <div className="cart-item-header">
+									<h3>{item.productId.title}</h3>
+										<p className="cart-price">${item.productId.price * item.quantity}</p>
+                                    </div>
+                                        <p className="cart-qty">Qty: {item.quantity}</p>
+								</div>
 							</div>
 						</div>
 					))
--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,33 +1,21 @@
 import React from "react";
+import './Cart.css'
 import CartItems from "../../components/CartItems/CartItems";
 import api from "../../axiosinstance";
 const Cart = (user) => {
 	const clearCart = () => {
 		console.log("increase product count");
 	};
-  const decreaseProdCount = () => {
-		try {
-      console.log("decrease prod count")
-		} catch (error) {
-			console.log("Error Decreasing Product Count");
-		}
-	};
-	const increaseProdCount = () => {
-		console.log("increase product count");
-	};
 	return (
     
 		<>
 			<div className="cart-wrapper">
-				Welcome To Your Cart, {user.user?.user}
+				<h2>Welcome To Your Cart, {user.user?.usernick}</h2>
 			</div>
       <div className="cart-item-wrapper">
       <CartItems user={user}/>
       </div>
-			<div className="cart-page-actions">
-				<button type="button">Clear Cart</button>
-        <button type="button">Place Order</button>
-			</div>
+
 		</>
 	);
 };
