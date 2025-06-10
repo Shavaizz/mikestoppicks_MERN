b800433b2b4f9a7b390e08da0cbcc0070ae4d9b7
Shavaiz <shaviz4444@gmail.com>
2025-06-10 18:11:00 +0500

    Final touchups and UI changes

--- frontend/src/components/searchBar/SearchBar.jsx
+++ frontend/src/components/searchBar/SearchBar.jsx
@@ -2,21 +2,17 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./SearchBar.css";
 const SearchBar = () => {
-	const [searchTerm, setSearchTerm] = useState(""); // State to store search term
-	const navigate = useNavigate(); // Hook to navigate programmatically
-
+	const [searchTerm, setSearchTerm] = useState(""); 
+	const navigate = useNavigate(); 
 	const handleChange = (e) => {
-		setSearchTerm(e.target.value); // Update the search term as user types
+		setSearchTerm(e.target.value); 
 	};
-
 	const handleButtonClick = () => {
 		if (searchTerm.trim()) {
-			// Navigate to search results page with search term as query parameter
 			navigate(`/search-results?query=${encodeURIComponent(searchTerm)}`);
 		}
 		setSearchTerm("");
 	};
-
 	return (
 		<>
 			<div id="input-wrapper">
@@ -28,7 +24,7 @@ const SearchBar = () => {
 					value={searchTerm}
 					onChange={handleChange}
 				/>
-				<button type="button" onClick={handleButtonClick}>
+				<button id="search-bar-click-button" type="button" onClick={handleButtonClick}>
 					Search
 				</button>
 			</div>
